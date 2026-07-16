926c9a5f09d817d53a1f8f836a119f829b7fcad0
MatthewMing11 <matthewm0102@gmail.com>
2026-07-16 17:59:00 -0400

    commented on file path dependency

--- tesseract.py
+++ tesseract.py
@@ -1,8 +1,10 @@
 import pytesseract
 from pdf2image import convert_from_path
 
+#update paths if downloaded in other locations
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-pages = convert_from_path("Sample Discharge Summary_0.pdf",poppler_path=r"C:\Program Files\poppler-26.02.0\Library\bin")
+pages = convert_from_path("Sample Discharge Summary_0.pdf",
+                          poppler_path=r"C:\Program Files\poppler-26.02.0\Library\bin")
 
 for i, page in enumerate(pages):
     text = pytesseract.image_to_string(page)
